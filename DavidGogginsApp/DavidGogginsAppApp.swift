a344d24a1a3f69b540bc7129e8b087aeb51f1236
Ruan Azevedo <ruan.azevedo@gmail.com>
2024-03-12 12:55:37 -0300

    Initial Commit

---
+++ DavidGogginsApp/DavidGogginsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DavidGogginsAppApp.swift
+//  DavidGogginsApp
+//
+//  Created by Ruan Azevedo on 2024-03-12.
+//
+
+import SwiftUI
+
+@main
+struct DavidGogginsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
